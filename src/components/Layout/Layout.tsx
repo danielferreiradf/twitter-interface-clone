fd51a63f6e661154c9da3d884434633f5a4f0f0d
danielferreiradf <danielferreiradk@gmail.com>
2020-07-25 21:03:31 +0100

    inital setup

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Main from "../Main/Main";
+import { Container, Wrapper } from "./Layout.styles";
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+        {/* <MenuBar /> */}
+        {/* <Main /> */}
+        {/* <SideBar /> */}
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Layout;
